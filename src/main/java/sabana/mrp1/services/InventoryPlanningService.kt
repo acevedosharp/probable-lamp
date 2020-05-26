894ae04e6218ad8aed612e8f522e744e9dbc8f1d
acevedosharp <acevedosharp@hotmail.com>
2020-05-26 01:36:24 -0500

    almost there

--- src/main/java/sabana/mrp1/services/InventoryPlanningService.kt
+++ src/main/java/sabana/mrp1/services/InventoryPlanningService.kt
@@ -1,10 +1,7 @@
 package sabana.mrp1.services
 
 import org.springframework.stereotype.Component
-import sabana.mrp1.entities.ComportamientoMes
-import sabana.mrp1.entities.Ingrediente
-import sabana.mrp1.entities.IngredienteProducto
-import sabana.mrp1.entities.RegistroVentas
+import sabana.mrp1.entities.*
 import sabana.mrp1.repositories.IngredienteRepository
 import sabana.mrp1.repositories.OrdenCompraMesRepository
 import sabana.mrp1.repositories.ProductoRepository
@@ -17,38 +14,67 @@ class InventoryPlanningService(
         private val ingredienteRepository: IngredienteRepository,
         private val productoRepository: ProductoRepository
 ) {
-    // Mes (0-11), Ingrediente, Cantidad
-    private val accumulations: List<Pair<Ingrediente, Int>> = ingredienteRepository.findAll().map { it to 0 }
+
+    inner class MonthlyIngredientWrapper(val mes: Int, val ingrediente: Ingrediente, val cantidad: Int) {
+        override fun toString(): String {
+            return "(Mo: $mes - Ing: $ingrediente - Q: $cantidad)"
+        }
+    }
 
     init {
-        val productoToVentas = registroVentasRepository.findAllByTiempoEquals("futuro").map {
-            it.producto to it.comportamientosMes
-        }
+        // Desired output is: Mes (index from 0-11), Ingrediente, Cantidad represented as Map<Int, Pair<Ingrediente, Int>>.
 
-        productoToVentas.forEach {
-            println(it)
-        }
+        // Step 1: We extract Producto and Comportamientos from the RegistroVentasRepository. Size is the number of Productos.
+        val compToProd: List<Pair<List<ComportamientoMes>, Producto>> =
+                registroVentasRepository
+                        .findAllByTiempoEquals("futuro")
+                        .map { it.comportamientosMes to it.producto }
 
-        productoToVentas.forEach { pair ->
-            pair.second.forEach { comportamiento ->
-                pair.first.ingredientesProducto.forEach {
-
-                    println("Mes ${comportamiento.mes}: ${pair.first.nombre} - ${pair.first.ingredientesProducto.map { it.cantidad*comportamiento.ventas to it.metricaIngrediente}}")
+        // Step 2: We flatten our collection to List<Pair<ComportamientoMes, Producto>>
+        val compToProdFlat: List<Pair<ComportamientoMes, Producto>> =
+                compToProd.flatMap { pair: Pair<List<ComportamientoMes>, Producto> ->
+                    pair.first.map {
+                        it to pair.second
+                    }
                 }
-            }
-        }
 
-//        productoToVentas.forEach { println(it) }
+        // Step 3: We map Comportamiento to the List<IngredienteProducto> that we extract from producto
+        val compToIngredientes: List<Pair<ComportamientoMes, List<IngredienteProducto>>> =
+                compToProdFlat.map { it.first to it.second.ingredientesProducto.toList() }
+
+
+        // Step 4: Reshape collection to List<MonthlyIngredientWrapper>, sorry, I don't understand this either!
+        val wrapper =
+                compToIngredientes
+                        .groupBy { it.first.mes } // Map(month, Pair(Comp, List(Ing)))
+                        .flatMap { entry: Map.Entry<Int, List<Pair<ComportamientoMes, List<IngredienteProducto>>>> ->
+                            entry.value.map { pair: Pair<ComportamientoMes, List<IngredienteProducto>> ->
+                                pair.second.map {
+                                    pair.first to it
+                                } // List<Pair<ComportamientoMes, IngredienteProducto>>
+                                        .map {
+                                            MonthlyIngredientWrapper(
+                                                    entry.key,
+                                                    it.second.ingrediente,
+                                                    (it.first.ventas * it.second.cantidad).toInt()
+                                            )
+                                        }
+                            }
+                        }.flatten()
+
+        // Step 5
+        val aggregatedWrappers =
+                wrapper
+
+//                        .groupBy { it.mes }
+
+
+
+        println(aggregatedWrappers)
+
     }
 
     fun planUsingCurrentPrediction(): Unit {
 
-        registroVentasRepository.findAll().forEach { registroVentas: RegistroVentas ->
-            registroVentas.comportamientosMes.forEach { comportamientoMes: ComportamientoMes ->
-                registroVentas.producto.ingredientesProducto.forEach { ingredienteProducto: IngredienteProducto ->
-
-                }
-            }
-        }
     }
 }
